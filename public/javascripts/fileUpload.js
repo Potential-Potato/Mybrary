30a034bb9e8d71ad749b351468161391426207df
Neil <ncagabriel02@gmail.com>
2024-03-25 14:09:25 +0800

    done css

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -8,16 +8,7 @@ FilePond.setOptions({
     stylePanelAspectRatio: 150/100,
     imageResizeTargetWidth: 100,    
     imageResizeTargetHeight: 150
-}
-    
-)
+})
 
-
-
-//   FilePond.setOptions({
-//     stylePanelAspectRatio: 150 / 100,
-//     imageResizeTargetWidth: 100,
-//     imageResizeTargetHeight: 150
-//   })
   
   FilePond.parse(document.body);
